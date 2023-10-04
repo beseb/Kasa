e4a0a5b0b423f241333fd915ae6486893f612f2f
Sebastien Escudero <sebastien.escudero@gmail.com>
2023-10-04 10:30:20 +0200

    erreur page ok, TODO  header/footer/erreur responsive

--- src/pages/Erreur.jsx
+++ src/pages/Erreur.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom'
 import '../style/pages/erreur.scss'
 
 function Erreur(){
 
-    return <div><h1>Oups ! Une erreur s'est produite ! 🙊 </h1></div>
+    return <div className='erreur'><h1 className='erreur__title'>404</h1>
+    <h2 className='erreur__text'>Oups ! La page que vous demandez n'existe pas.</h2>
+    <Link to="/" className='erreur__link'>Retourner sur la page d'accueil</Link>
+    </div>
 }
 
 export default Erreur
