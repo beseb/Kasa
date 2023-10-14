670c20fb66e236e5bbe450601a25c3e24b96666a
Sebastien Escudero <sebastien.escudero@gmail.com>
2023-10-14 23:54:36 +0200

    fixed navigate to error page, some css on error page

--- src/components/LocationsItem.jsx
+++ src/components/LocationsItem.jsx
@@ -7,68 +7,70 @@ import Location__Host from './Location__Host'
 import Location__Rating from './Location__Rating'
 import Slideshow from './Slideshow'
 import Collapse from './Collapse'
-import { useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
 import '../../public/data/logements.json'
 import { useEffect, useState } from 'react'
 
 function LocationsItem({ id }) {
-  // On récupère l'id via l'URL du logement recherche
-
+  
+  const [logement, setLogement] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const navigate = useNavigate()
+  
+  // On récupère l'id via l'URL du logement recherche en props
   // On fetch toutes les infos depuis le json
-
-  const [logements, setLogements] = useState([])
   useEffect(() => {
     fetch('/data/logements.json')
-    .then((response) => response.json())
-    .then((data) => {
-      setLogements(data)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-    
-    
-  }, [])
-  const logement = logements.find((element) => element.id == id)
-
-
-  const navigate = useNavigate()
-
-  // Si l'id n'est pas trouvé dans le fichier.json
-  if (!logement) {
-    navigate('/Erreur', { state: { message: 'Failed to find hosting id' } })
-    return null
-  } else {
-    return (
-      <div id="main">
-        <div className="location__main--wrapper" >
-          <Slideshow pictures={logement.pictures} />
-          <div className="location__TitleCity--container">
-            <h1 className="location--title">{logement.title}</h1>
-            <h2 className="location--city">{logement.location}</h2>
-          </div>
-          <Location__Tags tags={logement.tags} />
-          <Location__Host
-            name={logement.host.name}
-            picture={logement.host.picture}
+      .then((response) => response.json())
+      .then((data) => {
+        const foundLogement = data.find((element) => element.id === id)
+        if (!foundLogement) {
+          navigate('/Erreur', {
+            state: { message: 'Failed to find hosting id' },
+          })
+        } else {
+          setLogement(foundLogement)
+          setIsLoading(false)
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+        navigate('/Erreur', { state: { message: 'Failed to load data' } })
+      })
+  }, [id, navigate])
+  // isLoading empeche d'afficher des erreurs dans la console en attendant la fin du fetch
+  if (isLoading) {
+    return <div>Loading ....</div>
+  }
+  return (
+    <div id="main">
+      <div className="location__main--wrapper">
+        <Slideshow pictures={logement.pictures} />
+        <div className="location__TitleCity--container">
+          <h1 className="location--title">{logement.title}</h1>
+          <h2 className="location--city">{logement.location}</h2>
+        </div>
+        <Location__Tags tags={logement.tags} />
+        <Location__Host
+          name={logement.host.name}
+          picture={logement.host.picture}
+        />
+        <Location__Rating rating={logement.rating} />
+        <div className="collapses__section" id="collapses_host_page">
+          <Collapse
+            title="Description"
+            content={logement.description}
+            page="location__description"
           />
-          <Location__Rating rating={logement.rating} />
-          <div className="collapses__section" id='collapses_host_page'>
-            <Collapse
-              title="Description"
-              content={logement.description}
-              page="location__description"
-            />
-            <Collapse
-              title="Equipement"
-              content={logement.equipments}
-              page="location__equipment"
-            />
-          </div>
+          <Collapse
+            title="Equipement"
+            content={logement.equipments}
+            page="location__equipment"
+          />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default LocationsItem
--- src/pages/Erreur.jsx
+++ src/pages/Erreur.jsx
@@ -3,13 +3,20 @@ import '../style/pages/erreur.scss'
 // On importe le main scss car les marges sont écrites dans le main.scss
 import '/src/style/main.scss'
 
-function Erreur(){
-
-    return <div id='main'><div className="erreur">
-        <h1 className='erreur__title'>404</h1>
-        <h2 className='erreur__text'>Oups ! La page que vous demandez n'existe pas.</h2>
-        <Link to="/Accueil" className='erreur__link'>Retourner sur la page d'accueil</Link>
-    </div> </div>
+function Erreur() {
+  return (
+    <div id="main">
+      <div className="erreur">
+        <h1 className="erreur__title">404</h1>
+        <h2 className="erreur__text">
+          Oups ! La page que vous demandez n'existe pas.
+        </h2>
+        <Link to="/Accueil" className="erreur__link">
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Erreur
