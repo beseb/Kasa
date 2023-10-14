bbe25ff608dc2763d1fabaf384adc50a66195df3
Sebastien Escudero <sebastien.escudero@gmail.com>
2023-10-14 22:24:31 +0200

    fixed header & collapses css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,12 @@
 import '/src/style/components/banner.scss'
 // On importe le main scss car les marges sont écrites dans le main.scss
 import '/src/style/main.scss'
-import Banner__text from './Banner__text'
 import imgAccueil from '../assets/Banner/Image_source_1_Accueil.png'
 import imgAbout from '../assets/Banner/Image_source_2_About.png'
 
 // La propriété 'page' est attendue dans les props
 // eslint-disable-next-line react/prop-types
 function Banner({ page }) {
-  console.log(imgAccueil)
   return (
     <div
       className={`banner`}
@@ -18,7 +16,12 @@ function Banner({ page }) {
           page === 'accueil' ? `url(${imgAccueil})` : `url(${imgAbout})`,
       }}
     >
-      <Banner__text page={page} />
+      <h1
+        className="banner__text"
+        style={{ display: page === 'accueil' ? 'flex' : 'none' }}
+      >
+        Chez vous,<br className="mobile-break"></br> partout et ailleurs
+      </h1>
     </div>
   )
 }
