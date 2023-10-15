c8754f6d1f6f28b58dce206dd0256ee5d6ce16f1
Sebastien Escudero <sebastien.escudero@gmail.com>
2023-10-15 15:16:24 +0200

    new collapse ok ! css ok !

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,57 +1,49 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
 import '../style/components/collapse.scss'
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 
 function Collapse({ title, content }) {
   const [isOpen, setIsOpen] = useState(false)
-
   const toggleCollapse = () => {
     setIsOpen(!isOpen)
   }
-  
-  if (Array.isArray(content)) {
+
+  if (content && Array.isArray(content)) {
     return (
-      <div className={`collapse__container `}>
-        <div className="collapse__header" onClick={toggleCollapse}>
-          <h3>{title}</h3>
+      <section className="collapse">
+        <div className="header" onClick={toggleCollapse}>
+          {title}{' '}
           <FontAwesomeIcon
             icon={faChevronUp}
-            className="icons"
             style={{
-              transform: `rotate(${isOpen ? -180 : 0}deg)`,
-              transition: 'transform 0.5s ease',
+              transform: isOpen ? 'rotate(-180deg)' : 'rotate(0deg)',
+              transition: 'all 0.7s ease',
             }}
           />
         </div>
-        <div className={`collapse__content  ${isOpen ? 'open' : ''}` }>
+        <div className={isOpen ? 'content open' : 'content'}>
           <ul>
             {content.map((element, index) => {
               return <li key={index}>{element}</li>
             })}
           </ul>
         </div>
-      </div>
+      </section>
     )
   } else {
     return (
-      <div className={`collapse__container `}>
-        <div className="collapse__header" onClick={toggleCollapse}>
-          <h3>{title}</h3>
+      <section className="collapse">
+        <div className="header" onClick={toggleCollapse}>
+          {title}
           <FontAwesomeIcon
             icon={faChevronUp}
-            className="icons"
-            style={{
-              transform: `rotate(${isOpen ? -180 : 0}deg)`,
-              transition: 'transform 0.5s ease',
-            }}
+            style={{ transform: isOpen ? 'rotate(-180deg)' : 'rotate(0deg)' }}
           />
         </div>
-        <div className={`collapse__content  ${isOpen ? 'open' : ''}`}>
-          {content}
-        </div>
-      </div>
+        <div className={isOpen ? 'content open' : 'content'}>{content}</div>
+      </section>
     )
   }
 }
